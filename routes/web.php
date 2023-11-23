529aca1748a2572462f11c6b535bc733ce7ce739
mauroliguori <mauroligu2@gmail.com>
2023-11-23 02:43:55 -0300

    Frontend completo

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\TareaController;
+use App\Http\Controllers\UsuarioController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,3 +17,7 @@ Route::get("/tareas",[TareaController::class,"Listar"]);
 
 Route::get("/tareas/{d}",[TareaController::class,"Obtener"]);
 Route::get("/eliminarTareas/{d}",[TareaController::class,'Eliminar']);
+Route::post("/login",[UsuarioController::class,"Login"]);
+Route::get("/login", function () {
+    return view("login");
+});
